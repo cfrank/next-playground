5d9bd5ca84ef14d97da75b047846bb04ce1b9f57
Chris Frank <chris@cfrank.org>
2023-12-02 00:16:57 -0800

    add shadcn/ui
    Signed-off-by: Chris Frank <chris@cfrank.org>

---
+++ app/checkout/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <div className="hidden items-start justify-center gap-6 rounded-lg p-8 md:grid lg:grid-cols-2 xl:grid-cols-3">
+      <p>Hello world</p>
+    </div>
+  )
+}
